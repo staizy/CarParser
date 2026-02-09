c34a0cdc11d828b20fb8f5d0cadaba166210d0a1
Dmitry Krizo <staizyy@yandex.ru>
2026-02-09 11:04:10 +0700

    fix

--- main.py
+++ main.py
@@ -47,42 +47,25 @@ req = requests.get(linkrel)
 soup = BeautifulSoup(req.content, 'html.parser')
 
 # парсинг объявлений с последующим заполнением экземпляров класса CarElement
-for l in soup.find_all('div'): #css-1f68fiz - div.class of a car
-    carname = None
-    carprice = None
-    carlink = None
-    caryear = None
-    try:
-        if 'css-1f68fiz' in l.attrs.get('class'):
-            carname, caryear = l.h3.text.strip().split(', ')
-            carlink = l.a.get('href')
-            carprice = l.find('div', class_='_1wx3rbx4').text.replace('₽', '').replace(' ', '')
-        if carname is not None:
-            Cars.append(CarElement(carname, carprice, carlink, caryear))
-    except Exception as e:
-        pass
-Cars.sort("price")
-for i in Cars:
-    print(i)
-print(111111111111111111111)
-Cars.sort("price")
-for i in Cars:
-    print(i)
+for l in soup.find_all('div', {'class':'css-1f68fiz'}): #css-1f68fiz - div.class of a car
+    carname, caryear = l.h3.text.strip().split(', ')
+    carlink = l.a.get('href')
+    carprice = l.find('div', class_='_1wx3rbx4').text.replace('₽', '').replace(' ', '')
+    if carname is not None:
+        Cars.append(CarElement(carname, carprice, carlink, caryear))
 
-# # парсинг всех марок авто
-# brands = []
-# brands_links = []
-# a = soup.find_all("a", {"class": "frg44i6"})
-# for i in a:
-#     brands.append(i.string.strip())
-#     brands_links.append(i.get("href"))
-# a = soup.find_all("noscript")
-# for i in a[0].prettify().split("</a>"):
-#     try:
-#         brands.append(i.split('/">\n  ')[1].strip())
-#         brands_links.append(i.split('href="')[1].strip().split('">\n')[0].strip())
-#     except Exception as e:
-#         pass
+
+# парсинг всех марок авто
+linkrel1 = "https://auto.drom.ru/"
+req1 = requests.get(linkrel1)
+soup1 = BeautifulSoup(req1.content, 'html.parser')
+brands = [brand.text for brand in soup1.find_all("a", {"class": "frg44i6"})]
+brands_links = [brand.get("href") for brand in soup1.find_all("a", {"class": "frg44i6"})]
+if len(brands) >= 19: # !!! очень жидко, не сработает, если в марке модели ровно 19 моделей !!! можно поменять на .contains()
+    brands += [model.text.split('/">')[0].strip() for model in soup1.find_all("noscript")[0]]
+    brands_links += [model.get("href").strip() for model in soup1.find_all("noscript")[0]]
+for i in range(len(brands)):
+    print(brands[i], brands_links[i])
 #
 # print(brands_links[50])
 # linkrel1 = brands_links[50]
